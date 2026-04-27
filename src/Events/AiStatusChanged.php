bfb281c54c28fc951eea5d8466a9fe3e1671b1e5
Anwar Jahid <ajr.jahid@gmail.com>
2026-04-27 17:03:32 +0600

    fix missing import

--- src/Events/AiStatusChanged.php
+++ src/Events/AiStatusChanged.php
@@ -7,6 +7,8 @@ namespace Anwar\GunmaAgent\Events;
 use Anwar\GunmaAgent\Models\ChatSession;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
